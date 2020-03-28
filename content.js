024cba97ce13763c6b08b7e355048561bae0b36f
José <mailjmq@gmail.com>
2020-03-28 14:44:45 -0300

    Lista la funcion de buscar el indice de cada columna

--- content.js
+++ content.js
@@ -1,36 +1,77 @@
 var myTab = document.querySelector("#dgOfertaVenta"); //Hacer referencia a la tabla contenedora de las ofertas
 
-var ofertas = [{ "numeroOferta": null, "numeroOrden": null, "mercado": '', "versiones": [{ "hora": 0, "volumen": 0, "precio": 0 }] }];
+// var ofertas = [{ "numeroOferta": null, "numeroOrden": null, "mercado": '', "versiones": [{ "hora": 0, "volumen": 0, "precio": 0 }] }];
 var ofertasLocal = [{ "numeroOferta": null, "numeroOrden": null, "mercado": '', "versiones": [{ "hora": 0, "volumen": 0, "precio": 0 }] }];
 
-function leerTabla() {
+// function leerTabla() {
 
+//     if (myTab != null) {
+//         for (var i = 2; i <= myTab.rows.length; i++) {
+//             var a = encontrarColumna();
+//             var b = a + 1
+//             var c = a + 2
+//             var d = a + 3
+//             var e = a + 6
+//             var n = parseInt(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + a + ")").textContent);
+//             ofertas[n - 1] = {
+//                 "numeroOferta": parseInt(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + a + ")").textContent),
+//                 "numeroOrden": i - 1,
+//                 "mercado": document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + b + ")").textContent,
+//                 "versiones": [
+//                     {
+//                         "hora": dateToNumber(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + c + ")").textContent),
+//                         "volumen": parseInt(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + d + ")").textContent.split('.').join('')),
+//                         "precio": parseFloat(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + e + ")").textContent.split(',').join('.'))
+//                     }
+//                 ]
+//             }
+//         }
+//         // return ofertas
+//     } else {
+//         console.log("myTab es null")
+//     };
+// };
+
+//----------------Constructores de objetos--------------------------------------------------------
+function Oferta(numeroOferta, numeroOrden, mercado, versiones) {
+    this.numeroOferta = numeroOferta;
+    this.numeroOrden = numeroOrden;
+    this.mercado = mercado;
+    this.versiones = versiones;
+}
+
+function Version(hora, volumen, porcentaje, precio, precioGba) {
+    this.hora = hora;
+    this.volumen = volumen;
+    this.porcentaje = porcentaje;
+    this.precio = precio;
+    this.precioGba = precioGba;
+}
+
+function leerTabla() {
+    var ofertas = [];
     if (myTab != null) {
-        for (var i = 2; i <= myTab.rows.length; i++) {
-            var a = encontrarColumna();
-            var b = a + 1
-            var c = a + 2
-            var d = a + 3
-            var e = a + 6
-            var n = parseInt(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + a + ")").textContent);
-            ofertas[n - 1] = {
-                "numeroOferta": parseInt(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + a + ")").textContent),
-                "numeroOrden": i - 1,
-                "mercado": document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + b + ")").textContent,
-                "versiones": [
-                    {
-                        "hora": dateToNumber(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + c + ")").textContent),
-                        "volumen": parseInt(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + d + ")").textContent.split('.').join('')),
-                        "precio": parseFloat(document.querySelector("#dgOfertaVenta > tbody > tr:nth-child(" + i + ") > td:nth-child(" + e + ")").textContent.split(',').join('.'))
-                    }
-                ]
-            }
-        }
-        // return ofertas
-    } else {
-        console.log("myTab es null")
+        for (i = 0; i < myTab.rows.length - 1; i++) {
+            var oferta = new Oferta(
+                parseInt(myTab.firstElementChild.children[i + 1].children[2].innerText),
+                i + 1,
+                myTab.firstElementChild.children[i + 1].children[3].innerText,
+                [new Version(
+                    dateToNumber(myTab.firstElementChild.children[i + 1].children[4].innerText),
+                    parseInt(myTab.firstElementChild.children[i + 1].children[5].innerText.split('.').join('')),
+                    parseFloat(myTab.firstElementChild.children[i + 1].children[7].innerText.split(',').join('.')),
+                    parseFloat(myTab.firstElementChild.children[i + 1].children[8].innerText.split(',').join('.')),
+                    parseFloat(myTab.firstElementChild.children[i + 1].children[9].innerText.split(',').join('.'))
+                )]
+            )
+            ofertas.push(oferta);
+        };
     };
-};
+    return ofertas;
+}
+var ofertas = leerTabla();
+console.log(ofertas);
+//------------------------------------------------------------------------------------------------
 
 // localStorage.clear();
 leerTabla();
@@ -38,18 +79,18 @@ iniciarResumen();
 guardarOfertas(ofertas);
 leerLocal();
 
-function leerLocal(){
+function leerLocal() {
     ofertasLocal = JSON.parse(localStorage.getItem('ofertas'));
 };
 
 //console.log(ofertas)
 
-var t = 60;
-setInterval(() => {
-    t=t-1
-    console.log(decirHora() + " (" + t + ")");
-    actualizarResumen();
-}, 1000);
+// var t = 60;
+// setInterval(() => {
+//     t = t - 1
+//     console.log(decirHora() + " (" + t + ")");
+//     actualizarResumen();
+// }, 1000);
 
 
 function dateToNumber(fechaHora) {
@@ -108,8 +149,8 @@ var configObserver = {
 var observer = new MutationObserver(mutations => {
     // console.log(mutations);
 
-console.log(mutations);
-console.log(mutations[0].target);
+    console.log(mutations);
+    console.log(mutations[0].target);
 
     if (mutations[0].target != 'div#udtGridUpdater') {
 
@@ -167,16 +208,16 @@ observador.observe(procesando, configObservador);
 
 //---------------HORA ACTUAL-------------------------------------------------------------------
 
-function decirHora(){
+function decirHora() {
 
-var fecha= new Date()
-var horas= fecha.getHours()
-var minutos = fecha.getMinutes()
-var segundos = fecha.getSeconds()
- 
-var horaActual = numeral(horas).format('00') + ":" + numeral(minutos).format('00') + ":" + numeral(segundos).format('00');
+    var fecha = new Date()
+    var horas = fecha.getHours()
+    var minutos = fecha.getMinutes()
+    var segundos = fecha.getSeconds()
 
-return horaActual;
+    var horaActual = numeral(horas).format('00') + ":" + numeral(minutos).format('00') + ":" + numeral(segundos).format('00');
+
+    return horaActual;
 
 };
 
@@ -196,7 +237,7 @@ function pintarCambios(cambios) {
                 if (ofertasLocal[cambios[i] - 1].versiones[0].precio != ofertas[cambios[i] - 1].versiones[0].precio) {
                     resaltarOferta(cambios[i], 180) //AZUL
                     console.log('Oferta ' + cambios[i] + ' cambió el precio')
-                } else { 
+                } else {
                     console.log('Oferta ' + cambios[i] + ' sin cambios')
                 };
             };
@@ -293,27 +334,27 @@ function menorPrecio(cuenca) {
 };
 //-----------------INICIAR LA TABLA RESUMEN-----------------------------------------------------------------
 
-function iniciarResumen (){
+function iniciarResumen() {
 
-    var noaV=volumenTotal('NOROESTE');
-    var nqnV=volumenTotal('NEUQUEN');
-    var chuV=volumenTotal('CHUBUT');
-    var sczV=volumenTotal('SANTA CRUZ');
-    var tdfV=volumenTotal('TIERRA DEL FUEGO');
-    var totV=noaV+nqnV+chuV+sczV+tdfV
+    var noaV = volumenTotal('NOROESTE');
+    var nqnV = volumenTotal('NEUQUEN');
+    var chuV = volumenTotal('CHUBUT');
+    var sczV = volumenTotal('SANTA CRUZ');
+    var tdfV = volumenTotal('TIERRA DEL FUEGO');
+    var totV = noaV + nqnV + chuV + sczV + tdfV
 
-    var noaP=menorPrecio('NOROESTE');
-    var nqnP=menorPrecio('NEUQUEN');
-    var chuP=menorPrecio('CHUBUT');
-    var sczP=menorPrecio('SANTA CRUZ');
-    var tdfP=menorPrecio('TIERRA DEL FUEGO');
-    var totP=menorPrecio('*');
+    var noaP = menorPrecio('NOROESTE');
+    var nqnP = menorPrecio('NEUQUEN');
+    var chuP = menorPrecio('CHUBUT');
+    var sczP = menorPrecio('SANTA CRUZ');
+    var tdfP = menorPrecio('TIERRA DEL FUEGO');
+    var totP = menorPrecio('*');
 
     // console.log(noaP);
 
     var encabezado = document.querySelector("#tablaHeader2")
     encabezado.outerHTML +=
-`<div id="resumenVolumen" class="logo-nav-container">
+        `<div id="resumenVolumen" class="logo-nav-container">
     <h2 class="logo">Volúmenes</h2>
     <div>
         <table class="res">
@@ -363,27 +404,27 @@ function iniciarResumen (){
             <tbody>
                 <tr>
                     <td>NOA</td>
-                    <td id="noaP">${noaP.toLocaleString('de-ES',{minimumFractionDigits:4})}</td>
+                    <td id="noaP">${noaP.toLocaleString('de-ES', { minimumFractionDigits: 4 })}</td>
                 </tr>
                 <tr>
                     <td>NQN</td>
-                    <td id="nqnP">${nqnP.toLocaleString('de-ES',{minimumFractionDigits:4})}</td>
+                    <td id="nqnP">${nqnP.toLocaleString('de-ES', { minimumFractionDigits: 4 })}</td>
                 </tr>
                 <tr>
                     <td>CHU</td>
-                    <td id="chuP">${chuP.toLocaleString('de-ES',{minimumFractionDigits:4})}</td>
+                    <td id="chuP">${chuP.toLocaleString('de-ES', { minimumFractionDigits: 4 })}</td>
                 </tr>
                 <tr>
                     <td>SCZ</td>
-                    <td id="sczP">${sczP.toLocaleString('de-ES',{minimumFractionDigits:4})}</td>
+                    <td id="sczP">${sczP.toLocaleString('de-ES', { minimumFractionDigits: 4 })}</td>
                 </tr>
                 <tr>
                     <td>TDF</td>
-                    <td id="tdfP">${tdfP.toLocaleString('de-ES',{minimumFractionDigits:4})}</td>
+                    <td id="tdfP">${tdfP.toLocaleString('de-ES', { minimumFractionDigits: 4 })}</td>
                 </tr>
                 <tr>
                     <th>TOTAL</th>
-                    <th id="totP">${totP.toLocaleString('de-ES',{minimumFractionDigits:4})}</th>
+                    <th id="totP">${totP.toLocaleString('de-ES', { minimumFractionDigits: 4 })}</th>
                 </tr>
             </tbody>
         </table>
@@ -398,47 +439,47 @@ function iniciarResumen (){
 };
 
 //-----------------ACTUALIZAR LA TABLA RESUMEN-----------------------------------------------------------------
-function actualizarResumen(){
+function actualizarResumen() {
     //VOLUMENES
-    var volNoa=document.getElementById('noaV');
-    var volNqn=document.getElementById('nqnV');
-    var volChu=document.getElementById('chuV');
-    var volScz=document.getElementById('sczV');
-    var volTdf=document.getElementById('tdfV');
-    var volTot=document.getElementById('totV');
+    var volNoa = document.getElementById('noaV');
+    var volNqn = document.getElementById('nqnV');
+    var volChu = document.getElementById('chuV');
+    var volScz = document.getElementById('sczV');
+    var volTdf = document.getElementById('tdfV');
+    var volTot = document.getElementById('totV');
 
-    volNoa.textContent=volumenTotal('NOROESTE').toLocaleString('de-ES');
-    volNqn.textContent=volumenTotal('NEUQUEN').toLocaleString('de-ES');
-    volChu.textContent=volumenTotal('CHUBUT').toLocaleString('de-ES');
-    volScz.textContent=volumenTotal('SANTA CRUZ').toLocaleString('de-ES');
-    volTdf.textContent=volumenTotal('TIERRA DEL FUEGO').toLocaleString('de-ES');
-    volTot.textContent=volumenTotal('*').toLocaleString('de-ES');
+    volNoa.textContent = volumenTotal('NOROESTE').toLocaleString('de-ES');
+    volNqn.textContent = volumenTotal('NEUQUEN').toLocaleString('de-ES');
+    volChu.textContent = volumenTotal('CHUBUT').toLocaleString('de-ES');
+    volScz.textContent = volumenTotal('SANTA CRUZ').toLocaleString('de-ES');
+    volTdf.textContent = volumenTotal('TIERRA DEL FUEGO').toLocaleString('de-ES');
+    volTot.textContent = volumenTotal('*').toLocaleString('de-ES');
 
     //PRECIOS
-    var pNoa=document.getElementById('noaP')
-    var pNqn=document.getElementById('nqnP')
-    var pChu=document.getElementById('chuP')
-    var pScz=document.getElementById('sczP')
-    var pTdf=document.getElementById('tdfP')
-    var pTot=document.getElementById('totP')
+    var pNoa = document.getElementById('noaP')
+    var pNqn = document.getElementById('nqnP')
+    var pChu = document.getElementById('chuP')
+    var pScz = document.getElementById('sczP')
+    var pTdf = document.getElementById('tdfP')
+    var pTot = document.getElementById('totP')
 
-    pNoa.textContent=menorPrecio('NOROESTE').toLocaleString('de-ES',{minimumFractionDigits:4})
-    pNqn.textContent=menorPrecio('NEUQUEN').toLocaleString('de-ES',{minimumFractionDigits:4})
-    pChu.textContent=menorPrecio('CHUBUT').toLocaleString('de-ES',{minimumFractionDigits:4})
-    pScz.textContent=menorPrecio('SANTA CRUZ').toLocaleString('de-ES',{minimumFractionDigits:4})
-    pTdf.textContent=menorPrecio('TIERRA DEL FUEGO').toLocaleString('de-ES',{minimumFractionDigits:4})
-    pTot.textContent=menorPrecio('*').toLocaleString('de-ES',{minimumFractionDigits:4})
+    pNoa.textContent = menorPrecio('NOROESTE').toLocaleString('de-ES', { minimumFractionDigits: 4 })
+    pNqn.textContent = menorPrecio('NEUQUEN').toLocaleString('de-ES', { minimumFractionDigits: 4 })
+    pChu.textContent = menorPrecio('CHUBUT').toLocaleString('de-ES', { minimumFractionDigits: 4 })
+    pScz.textContent = menorPrecio('SANTA CRUZ').toLocaleString('de-ES', { minimumFractionDigits: 4 })
+    pTdf.textContent = menorPrecio('TIERRA DEL FUEGO').toLocaleString('de-ES', { minimumFractionDigits: 4 })
+    pTot.textContent = menorPrecio('*').toLocaleString('de-ES', { minimumFractionDigits: 4 })
 };
 //-------DOWNLOAD LOCAL STORAGE--------------------------------------------------------------------------
 
-    var _myArray = JSON.stringify(ofertasLocal, null, 2); //indentation in json format, human readable
-    var jsonLink = document.querySelector('.link-to-download-json'),
-        jsonBlob = new Blob([_myArray], { type: "octet/stream" }),
-        jsonName = 'ofertas.json',
-        jsonUrl = window.URL.createObjectURL(jsonBlob);
-    jsonLink.setAttribute('href', jsonUrl);
-    jsonLink.setAttribute('download', jsonName);
-    // vLink.click();
+var _myArray = JSON.stringify(ofertasLocal, null, 2); //indentation in json format, human readable
+var jsonLink = document.querySelector('.link-to-download-json'),
+    jsonBlob = new Blob([_myArray], { type: "octet/stream" }),
+    jsonName = 'ofertas.json',
+    jsonUrl = window.URL.createObjectURL(jsonBlob);
+jsonLink.setAttribute('href', jsonUrl);
+jsonLink.setAttribute('download', jsonName);
+// vLink.click();
 
 //-------EXPORTAR TABLA A CSV--------------------------------------------------------------------------
 (function exportTableToCSV() {
@@ -458,13 +499,13 @@ function actualizarResumen(){
 
 //-------BAJAR TABLA A CSV--------------------------------------------------------------------------
 
-        var csvLink = document.querySelector('.link-to-download-csv'),
-            csvBlob = new Blob([csv], {type: "text/csv"}),
-            scvName = 'ofertas.csv',
-            csvUrl = window.URL.createObjectURL(csvBlob);
-        csvLink.setAttribute('href', csvUrl);
-        csvLink.setAttribute('download', scvName);
- 
+var csvLink = document.querySelector('.link-to-download-csv'),
+    csvBlob = new Blob([csv], { type: "text/csv" }),
+    scvName = 'ofertas.csv',
+    csvUrl = window.URL.createObjectURL(csvBlob);
+csvLink.setAttribute('href', csvUrl);
+csvLink.setAttribute('download', scvName);
+
 //-------BAJAR TABLA A XLS--------------------------------------------------------------------------
 
 // var tableToExcel = (function () {
